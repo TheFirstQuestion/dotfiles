a1c8ce9dcda8a74464eaa19f36cb58354a0c5167
S. G. Opferman <sophieopferman6@gmail.com>
2022-06-20 18:32:27 -0400

    tested update script; removed unnecessary submodules

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,17 +3,15 @@ set -euo pipefail
 IFS=$'\n\t'
 
 echo 'Starting update script!'
+echo ""
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # Linux
     sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get dist-upgrade -y; apt-get autoremove -y; apt-get autoclean -y'
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # MacOS
-    brew update
+    brew update --auto-update
     brew upgrade
 fi
-
-# Update dotbot itself
-cd $HOME/dotfiles/dotbot && git fetch && git checkout origin/master
 
 echo 'Update script finished!'
