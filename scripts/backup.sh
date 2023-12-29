b3f7fa11bdec171e33191e1d457ffbbb27ac663f
Steven G. Opferman <steven.g.opferman@gmail.com>
2023-12-29 16:00:44 -0500

    brightness controls match volume; dunst styled; display profile disappears sooner

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,50 +1,52 @@
+source $SOURCE_ALL_FUNCTIONS
+
 diveIntoDir() {
-    # Identify git repo
-    if [[ $(git -C "$1" status 2>/dev/null) ]]; then
-        # Skip (for now)
-        return 0
+  # Identify git repo
+  if [[ $(git -C "$1" status 2>/dev/null) ]]; then
+    # Skip (for now)
+    return 0
+  fi
+
+  # Process the files in this directory
+  processFiles "$1"
+
+  # And recurse downwards
+  for thisDirPath in $(find "$1" -type d -maxdepth 1); do
+    nameOf1=$(basename $1)
+    thisDir=$(basename $thisDirPath)
+
+    # Compare to any ignored dirs
+    # Also prevent infinite loop -- don't call on the current directory
+    if [[ -n $(echo ".git node_modules compiled __pycache__ build .AppleDouble .npm .sass-cache .ipynb_checkpoints profile_default .AppleDB .AppleDesktop Network Trash Folder Temporary Items .apdisk $nameOf1" | grep "$thisDir") ]]; then
+      continue
     fi
 
-    # Process the files in this directory
-    processFiles "$1"
-
-    # And recurse downwards
-    for thisDirPath in $(find "$1" -type d -maxdepth 1); do
-        nameOf1=$(basename $1)
-        thisDir=$(basename $thisDirPath)
-
-        # Compare to any ignored dirs
-        # Also prevent infinite loop -- don't call on the current directory
-        if [[ -n $(echo ".git node_modules compiled __pycache__ build .AppleDouble .npm .sass-cache .ipynb_checkpoints profile_default .AppleDB .AppleDesktop Network Trash Folder Temporary Items .apdisk $nameOf1" | grep "$thisDir") ]]; then
-            continue
-        fi
-
-        diveIntoDir "$thisDirPath"
-    done
+    diveIntoDir "$thisDirPath"
+  done
 }
 
 processFiles() {
-    # for thisFilePath in $(find "$1" -type f -maxdepth 1)
-    # do
-    #     # sed to escape square brackets (via https://superuser.com/a/1383431)
-    #     thisFile=$(basename $thisFilePath | sed -e 's/\[/\\[/g' -e 's/\]/\\]/g')
-    #
-    #     # Compare to any ignored filenames
-    #     if [[ -n $(echo ".DS_Store ipython_config.py local.properties google-services.json .DocumentRevisions-V100 .fseventsd .Spotlight-V100 .TemporaryItems .Trashes .VolumeIcon.icns .com.apple.timemachine.donotpresent" | grep "$thisFile") ]]; then
-    #         continue
-    #     fi
-    #
-    #     rsync -hmPCa --dry-run "$thisFilePath"
-    # done
-    rsync -vhmPCa --dry-run --exclude-from="$DOTFILE_DIR/templates/gitignore" --exclude=".git/*" --exclude=".expo/*" "$1" "$DESTINATION"
+  # for thisFilePath in $(find "$1" -type f -maxdepth 1)
+  # do
+  #     # sed to escape square brackets (via https://superuser.com/a/1383431)
+  #     thisFile=$(basename $thisFilePath | sed -e 's/\[/\\[/g' -e 's/\]/\\]/g')
+  #
+  #     # Compare to any ignored filenames
+  #     if [[ -n $(echo ".DS_Store ipython_config.py local.properties google-services.json .DocumentRevisions-V100 .fseventsd .Spotlight-V100 .TemporaryItems .Trashes .VolumeIcon.icns .com.apple.timemachine.donotpresent" | grep "$thisFile") ]]; then
+  #         continue
+  #     fi
+  #
+  #     rsync -hmPCa --dry-run "$thisFilePath"
+  # done
+  rsync -vhmPCa --dry-run --exclude-from="$DOTFILE_DIR/templates/gitignore" --exclude=".git/*" --exclude=".expo/*" "$1" "$DESTINATION"
 }
 
 ############################## EXECUTION BEGINS HERE #############################
 
 # Confirm that Archive exists and is a valid directory
 if [ ! -d "$HOME/Archive" ]; then
-    echo -e "${RED}error: ~/Archive not found${NC}"
-    exit 1
+  echo -e "${RED}error: ~/Archive not found${NC}"
+  exit 1
 fi
 
 # Create the timestamped directory
@@ -56,42 +58,42 @@ exec > >(tee "${thisBackupLogDir}/00_script_output.txt") 2>&1
 
 # Check that (a) a drive is connected and (b) the computer is charging
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Mint
-    for i in /mnt/media/Drive*; do
-        if [ -d "$i" ]; then
-            DESTINATION="$i"
-            break
-        fi
-    done
-    # Fedora
-    # Mount the drive
-    # TODO: make more robust (not throw error if already mounted, mount more drives, etc)
-    udisksctl mount -b /dev/sda1
-    for i in /run/media/steven/Drive*; do
-        if [ -d "$i" ]; then
-            DESTINATION="$i"
-            break
-        fi
-    done
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # Adapted from https://apple.stackexchange.com/a/116435
-    isCharging=$(system_profiler SPPowerDataType | grep -A3 -B7 'Condition' | grep "Charging" | awk '{print $2}')
-    if [ $isCharging != "Yes" ]; then
-        echo 'error: computer is not currently charging'
-        exit 1
+  # Mint
+  for i in /mnt/media/Drive*; do
+    if [ -d "$i" ]; then
+      DESTINATION="$i"
+      break
     fi
-    for i in /Volumes/Drive*; do
-        if [ -d "$i" ]; then
-            DESTINATION="$i"
-            break
-        fi
-    done
+  done
+  # Fedora
+  # Mount the drive
+  # TODO: make more robust (not throw error if already mounted, mount more drives, etc)
+  udisksctl mount -b /dev/sda1
+  for i in /run/media/steven/Drive*; do
+    if [ -d "$i" ]; then
+      DESTINATION="$i"
+      break
+    fi
+  done
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  # Adapted from https://apple.stackexchange.com/a/116435
+  isCharging=$(system_profiler SPPowerDataType | grep -A3 -B7 'Condition' | grep "Charging" | awk '{print $2}')
+  if [ $isCharging != "Yes" ]; then
+    echo 'error: computer is not currently charging'
+    exit 1
+  fi
+  for i in /Volumes/Drive*; do
+    if [ -d "$i" ]; then
+      DESTINATION="$i"
+      break
+    fi
+  done
 fi
 
 # Confirm that DESTINATION exists and is valid
 if [ ! -d "$DESTINATION" ]; then
-    echo -e "${RED}error: Destination not found${NC}"
-    exit 1
+  echo -e "${RED}error: Destination not found${NC}"
+  exit 1
 fi
 
 # Log that we are starting, because it may take a while
@@ -142,10 +144,10 @@ envs+=" base"                      # Add base environment
 
 # Iterate through each environment
 for env in $envs; do
-    env_name=$(basename "$env")
-    conda activate "$env_name"
-    # Export the environment specifications to a YAML file
-    conda env export --name "$env_name" >"${thisBackupLogDir}/conda_environment_${env_name}.yml"
+  env_name=$(basename "$env")
+  conda activate "$env_name"
+  # Export the environment specifications to a YAML file
+  conda env export --name "$env_name" >"${thisBackupLogDir}/conda_environment_${env_name}.yml"
 done
 echo '✔ Backed up conda envs!'
 
